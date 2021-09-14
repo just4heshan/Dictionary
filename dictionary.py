8e825cc9771bd7ae44492386c774654fa758eaee
just4heshan <90320984+just4heshan@users.noreply.github.com>
2021-09-14 20:41:21 +1000

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,60 @@
+from os import linesep
+from tkinter import *
+import json
+from difflib import get_close_matches
+
+data = json.load(open("Dictionary/data.json", "r"))
+
+def dictionary():
+    if word.get() in data  :
+        t1.delete('1.0', END)
+        for result in data[word.get()]: 
+            t1.insert(END, result, '%s', ( linesep),'%s', (linesep ))
+        
+    
+    elif word.get().lower() in data:
+        t1.delete('1.0', END)
+        for result in data[word.get().lower()]:  
+            t1.insert(END,result, '%s', ( linesep),'%s', (linesep ))
+    
+    elif word.get().title() in data:
+        t1.delete('1.0', END)
+        for result in data[word.get().title()]:  
+            t1.insert(END, result, '%s', ( linesep),'%s', (linesep ))
+
+    elif word.get().upper() in data:
+        t1.delete('1.0', END)
+        for result in data[word.get().upper()]:  
+            t1.insert(END, result, '%s', ( linesep),'%s', (linesep ))
+    
+    elif len(get_close_matches(word.get(), data.keys())) > 0:
+        t1.delete('1.0', END)
+        t1.insert(END,"Did you mean \"%s\"" %get_close_matches(word.get(), data.keys()) [0])
+
+    else:
+        t1.delete('1.0', END)
+        t1.insert(END, "Try another word !")
+
+window = Tk(className='Python Examples - Window Color')
+window.configure(bg='#E0EEE0')
+
+
+
+l1 = Label(window, text='English Dictionary', font='Helvetica 28 bold italic', pady=20, padx=100,bg='#36648B', fg='#FFFFFF')
+l1.grid(row=0, column=0)
+l2 = Label(window, text='Type the word in following Search bar: ', font='Helvetica 12 bold', fg='#2F4F4F' )
+l2.grid(row=1, column=0)
+
+b1 = Button(window, text='Translate', command=dictionary, font='Helvetica 20 bold', bg='#76EEC6', fg='#2F4F4F')
+b1.grid(row=1, column=2, columnspan=5)
+
+word = StringVar()
+e1 = Entry(window, textvariable=word)
+e1.grid(row=1, column=1)
+
+t1=Text(window, height=20, width=80)
+t1.grid(row=2, column=0)
+
+window.mainloop()
+
+
